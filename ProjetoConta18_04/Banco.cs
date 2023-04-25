188878b24b082a7de02e1626aa2a4a45f13179d8
Gabriel Rodrigues Rolim <100931450+rolim265@users.noreply.github.com>
2023-04-25 09:31:59 -0300

    prj_emprestimo_banco

---
+++ ProjetoConta18_04/Banco.cs
@@ -0,0 +1,49 @@
+﻿namespace ProjetoConta18_04;
+class Banco
+{   
+    //Funções originais do banco
+
+    public int Numero {get; set;}
+    private double Saldo { get;  set;}
+    public double Limite {get; private set;}
+
+    public void AjustarLimite(double valor){
+        this.Limite = valor;
+    }
+    public void Depositar( double valor){
+        this.Saldo += valor;
+    }
+    public void Sacar(double valor){
+        if (this.Saldo + this.Limite <= valor){
+            Console.WriteLine("Você esta sacando mais do que você tem");
+        }else{
+             this.Saldo -= valor;
+        }
+    }
+    public double MostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    //Nova função de empréstimo
+
+    public double parcela {get; set;}
+    public double valor {get; set;}
+
+    public string emprestimo(int parcelas, double dinheiro){
+        this.parcela = parcelas;
+        this.valor = dinheiro;
+        
+        double div = dinheiro/parcelas;
+        double mult = dinheiro * 0.06;
+        double soma = mult + div;
+        double total = soma *parcelas;
+
+        string resultado = "O valor da sua parcela sem juros é: "+ div +". O valor da parcela com juros: "+ mult + ". O valor do juros com prestações é de: "+ soma +"e o valor total a pagar é de: "+total;
+        return resultado;
+        
+     
+    }
+    public void imprimi(string texto){
+        Console.WriteLine(texto);
+    }
+}
---
+++ ProjetoConta18_04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjetoConta18_04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Banco ETEC MCM");
+        Banco b = new Banco();
+        //c.Saldo = 1000;
+        b.Depositar(1000);
+        b.Sacar(1600);
+        //  com = é Set
+        // sem igual Get
+        b.AjustarLimite(500);
+
+        double valortotal = b.MostrarSaldo();
+
+        //Console.WriteLine("Seu saldo é de " + c.Saldo + " Com limite de " + c.Limite);
+
+        Console.WriteLine("Seu saldo total é de " + valortotal);
+        //Console.WriteLine("Seu saldo total é de " + c.Saldo);
+
+        b.imprimi(b.emprestimo(12,1000));
+    }
+}
